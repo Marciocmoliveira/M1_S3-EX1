4d531abb4afa754a90419a452a03d8ea9df823b4
Marciocmoliveira <critianomarcio@hotmail.com>
2023-02-20 20:52:24 -0300

    Adicionar arquivos de projeto.

---
+++ M1S3Ex1/Program.cs
@@ -0,0 +1,7 @@
+﻿using M1S3Ex1;
+
+Produto produto1 = new Produto("calculadora", 5.99, 199);
+Produto produto2 = new Produto();
+
+Console.WriteLine($" O produto tem nome: {produto1.nome}, valor: {produto1.preco}, quantidade: {produto1.quantidade}" );
+Console.WriteLine($" O produto tem nome: {produto2.nome}, valor: {produto2.preco}, quantidade: {produto2.quantidade}");
